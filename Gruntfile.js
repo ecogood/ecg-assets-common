0c794c7426db77f7ddd00e1b2642df7d4f9e8bbc
Nikolay Georgiev <nikolay@georgiev.io>
2014-10-27 02:58:48 +0100

    feat(css): create ecg and general CSS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,33 @@
 
 module.exports = function(grunt) {
 
+  // Configurable paths for the application
+  var appConfig = {
+    assets: 'assets/'
+  };
+
   // Project configuration.
   grunt.initConfig({
+
+    // Project settings
+    dir: appConfig,
+
+    sass: {
+      options: {
+        sourceMap: false
+      },
+      ecg: {
+        files: {
+          '<%= dir.assets %>/css/ecg.css': '<%= dir.assets %>/sass/ecg.scss'
+        }
+      },
+      general: {
+        files: {
+          '<%= dir.assets %>/css/general.css': '<%= dir.assets %>/sass/general.scss'
+        }
+      }
+    },
+
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
@@ -23,7 +48,13 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the 'uglify' task.
+  /* Load tasks */
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-bump');
+
+
+  grunt.registerTask('default', [
+    'sass'
+  ]);
 
 };
